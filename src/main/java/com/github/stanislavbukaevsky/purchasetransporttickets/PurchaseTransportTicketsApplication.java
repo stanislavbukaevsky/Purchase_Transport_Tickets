55f3e2972a23466a7e5309356470e28edbfb8a04
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2023-09-15 16:14:54 +0300

    Первый коммит

---
+++ src/main/java/com/github/stanislavbukaevsky/purchasetransporttickets/PurchaseTransportTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.github.stanislavbukaevsky.purchasetransporttickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseTransportTicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurchaseTransportTicketsApplication.class, args);
+    }
+
+}
